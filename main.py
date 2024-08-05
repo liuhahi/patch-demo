a1dd28aebd528a913e48386f48cb59f9b7afb356
zeyan <liuzeyan@scantist.com>
2024-08-05 23:15:57 +0800

    fix: typo

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def claude(full_prompt):
     client = AnthropicVertex(region="us-east5", project_id=PROJECT_ID)
     # for 3.5 claude-3-5-sonnet@20240620
     message = client.messages.create(
-        model="claude-3-sonnet@20240229"
+        model="claude-3-sonnet@20240229",
         system="You are a seasoned software developer, working in a Tech Unicorn.",
         max_tokens=4096,
         temperature=0,
